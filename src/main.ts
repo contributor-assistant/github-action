b7bb4238ec70b4febb8e8ec5bd2451766477dd2f
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2020-01-13 15:16:02 +0100

    Added debug statement

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,28 @@
-import * as core from '@actions/core'
-import { context } from '@actions/github'
-const io = require('@actions/io')
-import { getclas } from './checkcla'
-import { lockPullRequest } from './pullRequestLock'
-
-
+import * as core from "@actions/core"
+import { context } from "@actions/github"
+const io = require("@actions/io")
+import { getclas } from "./checkcla"
+import { lockPullRequest } from "./pullRequestLock"
 
 export async function run() {
   try {
     const pullRequestNo: number = context.issue.number
-    //s   console.log("the context is " + JSON.stringify(context, null, 2))
-    core.info('CLA Assistant GitHub Action has started')
-    core.info('the PR No is ' + JSON.stringify(pullRequestNo))
-    if (context.payload.action === 'closed') {
+    core.debug(
+      "checking the context of the commented user " +
+        JSON.stringify(context.payload.comment.user, null, 2)
+    )
+    core.info("CLA Assistant GitHub Action has started")
+    core.info("the PR No is " + JSON.stringify(pullRequestNo))
+    if (context.payload.action === "closed") {
       return lockPullRequest(pullRequestNo)
-    }
-    else {
+    } else {
       await getclas(pullRequestNo)
 
       // const rateLimit = await octokit.rateLimit.get()
       // console.log(JSON.stringify(rateLimit.data.resources, null, 4))
     }
-
   } catch (error) {
     core.setFailed(error.message)
   }
-
 }
 run()
