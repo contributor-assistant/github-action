72c266d02dcf188be0f1a4f57d8c12861d7b9f99
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2020-07-28 09:27:30 +0200

    refactor and clean up

--- src/url.ts
+++
@@ -1,8 +0,0 @@
-import * as core from '@actions/core'
-
-
-export function pathToCLADocument() {
-    const pathToCLADocument = core.getInput('path-to-cla-document')
-    core.info(`pathToCLADocument ----> ${pathToCLADocument}`)
-    return pathToCLADocument
-}
