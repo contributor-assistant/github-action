9b378b9b4481fa8147c2220cd9c5dca2e5569ee0
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2020-08-05 15:48:11 +0200

    Feat: remote repo support

--- src/octokit.ts
+++ src/octokit.ts
@@ -1,10 +1,10 @@
 import { getOctokit } from '@actions/github'
 
-const tokenToRemoteRepository = process.env.REMOTE_REPOSITORY_TOKEN
 const githubActionsDefaultToken = process.env.GITHUB_TOKEN
+const tokenToRemoteRepository = process.env.REMOTE_REPOSITORY_TOKEN
 
 export const octokit = getOctokit(githubActionsDefaultToken as string)
-export const octokitUsingPAT = getOctokit(githubActionsDefaultToken as string)
+export const octokitUsingPAT = getOctokit(tokenToRemoteRepository as string)
 
 export function isTokenToRemoteRepositoryPresent(): boolean {
     return (typeof tokenToRemoteRepository !== "undefined") || tokenToRemoteRepository !== ''
