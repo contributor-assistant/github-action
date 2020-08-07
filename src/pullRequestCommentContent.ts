d3b7f6a718e70368e2c63e76346dc2f4b3d74e15
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2020-08-07 15:42:38 +0200

    fix: pr comment wrong spacing

--- src/pullRequestCommentContent.ts
+++ src/pullRequestCommentContent.ts
@@ -17,11 +17,9 @@ export function commentContent(signed: boolean, committerMap: CommitterMap): str
     }
 
     let you = committersCount > 1 ? `you all` : `you`
-    let lineOne = (input.getCustomNotSignedPrComment() || '<br/>Thank you for your submission, we really appreciate it. Like many open-source projects, we ask that $you sign our [Contributor License Agreement]($pathToCLADocument) before we can accept your contribution. You can sign the CLA by just posting a Pull Request Comment same as the below format.').replace('$pathToCLADocument', input.getPathToCLADocument()).replace('$you', you)
-    let text = `**CLA Assistant Lite bot:** <br/> ${lineOne}
-    - - -
-    ***I have read the CLA Document and I hereby sign the CLA***
-    - - - 
+    let lineOne = (input.getCustomNotSignedPrComment() || '<br/>Thank you for your submission, we really appreciate it. Like many open-source projects, we ask that $you sign our [Contributor License Agreement]($pathToCLADocument) before we can accept your contribution. You can sign the CLA by just posting a Pull Request Comment same as the below format.  <br/> ').replace('$pathToCLADocument', input.getPathToCLADocument()).replace('$you', you)
+    let text = `**CLA Assistant Lite bot:** ${lineOne} - - -
+    ***I have read the CLA Document and I hereby sign the CLA*** <br/> - - - 
     <br/>
     `
 
