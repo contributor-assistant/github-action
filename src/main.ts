19fa28e99527ed1b026befd2cebd4612f1acc0be
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2020-08-14 22:29:48 +0200

    debug recheck last failed workflow run

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { context } from '@actions/github'
 import { getclas } from './checkcla'
 import { lockPullRequest } from './pullRequestLock'
+import { octokit } from './octokit'
 
 import * as core from '@actions/core'
 
 export async function run() {
   try {
-    core.info(context.workflow)
 
+    const selfWorkFlowId: any = await getSelfWorkflowId()
+    core.info(selfWorkFlowId)
     core.info(`CLA Assistant GitHub Action bot has started the process`)
 
     if (context.payload.action === 'closed') {
@@ -21,3 +23,18 @@ export async function run() {
   }
 }
 run()
+
+async function getSelfWorkflowId(): Promise<number> {
+  const workflowList = await octokit.actions.listRepoWorkflows({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+  });
+
+  const workflow = workflowList.data.workflows
+    .find(w => w.name == context.workflow)
+
+  if (!workflow) {
+    throw new Error(`Unable to locate this workflow's ID in this repository, can't retrigger job..`)
+  }
+  return workflow.id
+}
