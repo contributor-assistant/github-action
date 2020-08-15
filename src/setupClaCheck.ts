31702144a0d474985c113e86b6102a799fedd957
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2020-08-15 23:41:16 +0200

    refactoring: setupClaCheck

--- src/setupClaCheck.ts
+++ src/setupClaCheck.ts
@@ -113,18 +113,18 @@ async function getCLAFileContentandSHA(committers: CommittersDetails[], committe
       path: input.getPathToSignatures(),
       ref: input.getBranch()
     })
-    const sha = result?.data?.sha
-    const claFileContentString = Buffer.from(result.data.content, 'base64').toString()
-    const claFileContent = JSON.parse(claFileContentString)
-    return { claFileContent, sha }
   } catch (error) {
     if (error.status === 404) {
       // await createClaFileAndPRComment(committers, committerMap, pullRequestNo)
-      throw new Error(`Committers of pull request ${context.issue.number} have to sign the CLA`)
+      core.setFailed(`Committers of pull request ${context.issue.number} have to sign the CLA`)
     } else {
       core.setFailed(`Could not retrieve repository contents: ${error.message}. Status: ${error.status || 'unknown'}`)
     }
   }
+  const sha = result?.data?.sha
+  const claFileContentString = Buffer.from(result.data.content, 'base64').toString()
+  const claFileContent = JSON.parse(claFileContentString)
+  return { claFileContent, sha }
 }
 
 // TODO: refactor the commit message when a project admin does recheck PR
