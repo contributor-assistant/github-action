f961a89422a88691e68b16bd002318a6c2d4f4bf
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2020-08-15 23:46:01 +0200

    refactoring: setupClaCheck

--- src/setupClaCheck.ts
+++ src/setupClaCheck.ts
@@ -21,42 +21,42 @@ export async function setupClaCheck() {
   let committers = await getCommitters() as CommittersDetails[]
   committers = checkAllowList(committers) as CommittersDetails[]
 
-  const { claFileContent, sha } = await getCLAFileContentandSHA(committers, committerMap, pullRequestNo)
+  await getCLAFileContentandSHA(committers, committerMap, pullRequestNo)
 
-  committerMap = prepareCommiterMap(committers, claFileContent) as CommitterMap
+  // committerMap = prepareCommiterMap(committers, claFileContent) as CommitterMap
 
-  if (committerMap?.notSigned && committerMap?.notSigned.length === 0) {
-    signed = true
-  }
-  try {
-    const reactedCommitters: any = (await prComment(signed, committerMap, committers, pullRequestNo)) as ReactedCommitterMap
+  // if (committerMap?.notSigned && committerMap?.notSigned.length === 0) {
+  //   signed = true
+  // }
+  // try {
+  //   const reactedCommitters: any = (await prComment(signed, committerMap, committers, pullRequestNo)) as ReactedCommitterMap
 
-    if (signed) {
-      core.info(`All committers have signed the CLA`)
-      return reRunLastWorkFlowIfRequired()
-    }
-    if (reactedCommitters?.newSigned.length) {
-      claFileContent.signedContributors.push(...reactedCommitters.newSigned)
-      let contentString = JSON.stringify(claFileContent, null, 2)
-      let contentBinary = Buffer.from(contentString).toString("base64")
-      /* pushing the recently signed  contributors to the CLA Json File */
-      await updateFile(sha, contentBinary, pullRequestNo)
-    }
-    if (reactedCommitters?.allSignedFlag) {
-      core.info(`All contributors have signed the CLA`)
-      return reRunLastWorkFlowIfRequired()
-    }
+  //   if (signed) {
+  //     core.info(`All committers have signed the CLA`)
+  //     return reRunLastWorkFlowIfRequired()
+  //   }
+  //   if (reactedCommitters?.newSigned.length) {
+  //     claFileContent.signedContributors.push(...reactedCommitters.newSigned)
+  //     let contentString = JSON.stringify(claFileContent, null, 2)
+  //     let contentBinary = Buffer.from(contentString).toString("base64")
+  //     /* pushing the recently signed  contributors to the CLA Json File */
+  //     await updateFile(sha, contentBinary, pullRequestNo)
+  //   }
+  //   if (reactedCommitters?.allSignedFlag) {
+  //     core.info(`All contributors have signed the CLA`)
+  //     return reRunLastWorkFlowIfRequired()
+  //   }
 
-    /* return when there are no unsigned committers */
-    if (committerMap.notSigned === undefined || committerMap.notSigned.length === 0) {
-      core.info(`All contributors have signed the CLA`)
-      return reRunLastWorkFlowIfRequired()
-    } else {
-      core.setFailed(`committers of Pull Request number ${context.issue.number} have to sign the CLA`)
-    }
-  } catch (err) {
-    core.setFailed(`Could not update the JSON file: ${err.message}`)
-  }
+  //   /* return when there are no unsigned committers */
+  //   if (committerMap.notSigned === undefined || committerMap.notSigned.length === 0) {
+  //     core.info(`All contributors have signed the CLA`)
+  //     return reRunLastWorkFlowIfRequired()
+  //   } else {
+  //     core.setFailed(`committers of Pull Request number ${context.issue.number} have to sign the CLA`)
+  //   }
+  // } catch (err) {
+  //   core.setFailed(`Could not update the JSON file: ${err.message}`)
+  // }
 
 }
 
@@ -121,10 +121,10 @@ async function getCLAFileContentandSHA(committers: CommittersDetails[], committe
       core.setFailed(`Could not retrieve repository contents: ${error.message}. Status: ${error.status || 'unknown'}`)
     }
   }
-  const sha = result?.data?.sha
-  const claFileContentString = Buffer.from(result.data.content, 'base64').toString()
-  const claFileContent = JSON.parse(claFileContentString)
-  return { claFileContent, sha }
+  // const sha = result?.data?.sha
+  // const claFileContentString = Buffer.from(result.data.content, 'base64').toString()
+  // const claFileContent = JSON.parse(claFileContentString)
+  // return { claFileContent, sha }
 }
 
 // TODO: refactor the commit message when a project admin does recheck PR
