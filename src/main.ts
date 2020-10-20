45bcaa8b26c668a0f00fa73aa9595514b8d1799f
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2020-10-20 15:59:55 +0200

    debug: rerunworkflow

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { context } from '@actions/github'
 import { setupClaCheck } from './setupClaCheck'
 import { lockPullRequest } from './pullrequest/pullRequestLock'
 
+import * as figlet from 'figlet'
+
 import * as core from '@actions/core'
 
 
 
 export async function run() {
   try {
+    figlet('Cla Assistant', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      console.log(data)
+  });
     core.info(`CLA Assistant GitHub Action bot has started the process`)
 
     if (context.payload.action === 'closed') {
