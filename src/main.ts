a15401a3eeb961dcbfa2aae691ab20d6b5e22ddd
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2020-10-20 16:24:19 +0200

    adding ascii text

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,25 @@ import { context } from '@actions/github'
 import { setupClaCheck } from './setupClaCheck'
 import { lockPullRequest } from './pullrequest/pullRequestLock'
 
-var figlet = require('figlet')
-
+import * as util from 'util'
+import * as  figlet from 'figlet'
 import * as core from '@actions/core'
+
 
 
 
 export async function run() {
   try {
-    figlet('Cla Assistant', function(err, data) {
-      if (err) {
-          console.log('Something went wrong...');
-          console.dir(err);
-          return;
-      }
-      console.log(data)
-  });
+    const text = await figlet('CLA ASSISTANT')
+    console.log(text)
+  //   figlet('Cla Assistant', function(err, data) {
+  //     if (err) {
+  //         console.log('Something went wrong...');
+  //         console.dir(err);
+  //         return;
+  //     }
+  //     console.log(data)
+  // });
     core.info(`CLA Assistant GitHub Action bot has started the process`)
 
     if (context.payload.action === 'closed') {
