8fe6ee4a55be8eecda24911682fc647fa0821aed
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2022-06-30 12:14:47 +0200

    debug pat

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,19 @@ import { lockPullRequest } from './pullrequest/pullRequestLock'
 
 import * as core from '@actions/core'
 import * as input from './shared/getInputs'
+import { isPersonalAccessTokenPresent } from './octokit';
 
 
 
 export async function run() {
   try {
     core.info(`CLA Assistant GitHub Action bot has started the process`)
-    core.warning(`Test warning`)
-    
+    if (!isPersonalAccessTokenPresent()) {
+      core.warning(`Test warning----`)
+      core.setFailed('Please enter a personal access token as a environment variable in the CLA workflow file as described in the https://github.com/cla-assistant/github-action documentation')
+      return
+    }
+
     /*
     * using a `string` true or false purposely as github action input cannot have a boolean value
     */
