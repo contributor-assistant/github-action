d707083f73b759258bd1fd65d6a37edf5e5f60af
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2022-07-05 16:47:41 +0200

    dev: error-handling-fork

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,11 @@ import { isPersonalAccessTokenPresent } from './octokit';
 export async function run() {
   try {
     core.info(`CLA Assistant GitHub Action bot has started the process`)
+    if (!isPersonalAccessTokenPresent()) {
+      core.warning(`Test warning----`)
+      core.setFailed('Please enter a personal access token as a environment variable in the CLA workflow file as described in the https://github.com/cla-assistant/github-action documentation')
+      return
+    }
 
     /*
     * using a `string` true or false purposely as github action input cannot have a boolean value
