f2094f5ef4e491f45965fc95313170205ca0d980
Silvio <silviogutierrez@gmail.com>
2022-09-28 00:17:17 -0400

    Add owner and repo variables to commit message template

--- src/persistence/persistence.ts
+++ src/persistence/persistence.ts
@@ -38,15 +38,23 @@ export async function updateFile(sha: string, claFileContent, reactedCommitters:
     claFileContent?.signedContributors.push(...reactedCommitters.newSigned)
     let contentString = JSON.stringify(claFileContent, null, 2)
     let contentBinary = Buffer.from(contentString).toString("base64")
+    const owner = input.getRemoteOrgName() || context.repo.owner;
+    const repo = input.getRemoteRepoName() || context.repo.repo;
+  
     await octokitInstance.repos.createOrUpdateFileContents({
-        owner: input.getRemoteOrgName() || context.repo.owner,
-        repo: input.getRemoteRepoName() || context.repo.repo,
-        path: input.getPathToSignatures(),
-        sha,
-        message: input.getSignedCommitMessage() ?
-            input.getSignedCommitMessage().replace('$contributorName', context.actor).replace('$pullRequestNo', context.issue.number.toString()) :
-            `@${context.actor} has signed the CLA from Pull Request #${pullRequestNo}`,
-        content: contentBinary,
-        branch: input.getBranch()
-    })
+      owner,
+      repo,
+      path: input.getPathToSignatures(),
+      sha,
+      message: input.getSignedCommitMessage()
+        ? input
+            .getSignedCommitMessage()
+            .replace("$contributorName", context.actor)
+            .replace("$pullRequestNo", context.issue.number.toString())
+            .replace("$owner", owner)
+            .replace("$repo", repo)
+        : `@${context.actor} has signed the CLA from Pull Request #${pullRequestNo}`,
+      content: contentBinary,
+      branch: input.getBranch(),
+    });
 }
