b6d72c0c85be3d616e0792ae3c9862e465d6504e
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2023-02-07 22:32:20 +0100

    dev :hammer:

--- src/octokit.ts
+++ src/octokit.ts
@@ -1,12 +1,16 @@
-import { getOctokit } from '@actions/github'
+import {getOctokit} from '@actions/github'
 
 const githubActionsDefaultToken = process.env.GITHUB_TOKEN
 const personalAccessToken = process.env.PERSONAL_ACCESS_TOKEN as string
 
 export const octokit = getOctokit(githubActionsDefaultToken as string)
-export const octokitUsingPAT = isPersonalAccessTokenPresent() ? getOctokit(personalAccessToken as string) : octokit
+export const octokitUsingPAT = isPersonalAccessTokenPresent()
+  ? getOctokit(personalAccessToken as string)
+  : octokit
 
 export function isPersonalAccessTokenPresent(): boolean {
-    return personalAccessToken !== ""
+  if (personalAccessToken !== '') {
+    console.log('I am called!!', personalAccessToken)
+  }
+  return personalAccessToken !== ''
 }
-
