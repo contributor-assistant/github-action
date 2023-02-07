51b6f58b2aa9f7b4a1e072629d0e358702f68a22
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2023-02-07 23:39:20 +0100

    debug: :hammer:

--- src/octokit.ts
+++ src/octokit.ts
@@ -1,7 +1,7 @@
 import { getOctokit } from "@actions/github"
 
 const githubActionsDefaultToken = process.env.GITHUB_TOKEN
-const personalAccessToken = process.env.PERSONAL_ACCESS_TOKEN as string
+//const personalAccessToken = process.env.PERSONAL_ACCESS_TOKEN as string
 
 export const octokit = getOctokit(githubActionsDefaultToken as string)
 /*export const octokitUsingPAT = isPersonalAccessTokenPresent()
