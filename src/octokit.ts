4771ba41ffc1e1ac8a1ddf45a72175771de183b8
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2023-02-08 16:18:00 +0100

    debug: checks if the PAT is present :hammer:

--- src/octokit.ts
+++ src/octokit.ts
@@ -1,10 +1,11 @@
 import { getOctokit } from '@actions/github'
+import * as core from '@actions/core'
 
 const githubActionsDefaultToken = process.env.GITHUB_TOKEN
 export const personalAccessToken = process.env.PERSONAL_ACCESS_TOKEN as string
 
 export const octokit = getOctokit(githubActionsDefaultToken as string)
-/*export const octokitUsingPAT = getOctokit(personalAccessToken as string)
+/*export const octokitUsingPAT = getOctokit(personalAccessToken as string)*/
 
 export function isPersonalAccessTokenPresent() {
   if (!process.env.PERSONAL_ACCESS_TOKEN) {
@@ -12,4 +13,4 @@ export function isPersonalAccessTokenPresent() {
       'Please enter a personal access token "PERSONAL_ACCESS_TOKEN" as a environment variable with repo scope for storing signatures in a remote repository!'
     )
   }
-}*/
+}
