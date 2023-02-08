3cabe401fcf953d03cdcf841778ce50867b3b988
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2023-02-08 17:30:37 +0100

    debug: checks if the PAT is present :hammer:

--- dist/index.js
+++ dist/index.js
@@ -157,52 +157,23 @@ exports.default = _default;
 
 "use strict";
 
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.isPersonalAccessTokenPresent = exports.octokit = exports.personalAccessToken = void 0;
 const github_1 = __webpack_require__(469);
-const core = __importStar(__webpack_require__(470));
 const githubActionsDefaultToken = process.env.GITHUB_TOKEN;
 exports.personalAccessToken = process.env.PERSONAL_ACCESS_TOKEN;
 exports.octokit = (0, github_1.getOctokit)(githubActionsDefaultToken);
+/*export async function isPersonalAccessTokenPresent() {
+  if (!process.env.PERSONAL_ACCESS_TOKEN) {
+    console.log(process.env.PERSONAL_ACCESS_TOKEN)
+    core.setFailed(
+      'Please enter a personal access token "PERSONAL_ACCESS_TOKEN" as a environment variable with repo scope for storing signatures in a remote repository!'
+    )
+  }
+}*/
 function isPersonalAccessTokenPresent() {
-    return __awaiter(this, void 0, void 0, function* () {
-        if (!process.env.PERSONAL_ACCESS_TOKEN) {
-            console.log(process.env.PERSONAL_ACCESS_TOKEN);
-            core.setFailed('Please enter a personal access token "PERSONAL_ACCESS_TOKEN" as a environment variable with repo scope for storing signatures in a remote repository!');
-        }
-    });
+    console.log(process.env.PERSONAL_ACCESS_TOKEN);
+    return exports.personalAccessToken !== '';
 }
 exports.isPersonalAccessTokenPresent = isPersonalAccessTokenPresent;
 
--- src/octokit.ts
+++ src/octokit.ts
@@ -1,16 +1,20 @@
 import { getOctokit } from '@actions/github'
-import * as core from '@actions/core'
 
 const githubActionsDefaultToken = process.env.GITHUB_TOKEN
 export const personalAccessToken = process.env.PERSONAL_ACCESS_TOKEN as string
 
 export const octokit = getOctokit(githubActionsDefaultToken as string)
 
-export async function isPersonalAccessTokenPresent() {
+/*export async function isPersonalAccessTokenPresent() {
   if (!process.env.PERSONAL_ACCESS_TOKEN) {
     console.log(process.env.PERSONAL_ACCESS_TOKEN)
     core.setFailed(
       'Please enter a personal access token "PERSONAL_ACCESS_TOKEN" as a environment variable with repo scope for storing signatures in a remote repository!'
     )
   }
+}*/
+
+export function isPersonalAccessTokenPresent(): boolean {
+  console.log(process.env.PERSONAL_ACCESS_TOKEN)
+  return personalAccessToken !== ''
 }
