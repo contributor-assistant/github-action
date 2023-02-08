341da234ce2883e705fa71d1153849679efc63ea
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2023-02-08 16:14:59 +0100

    debug: checks if the PAT is present :hammer:

--- dist/index.js
+++ dist/index.js
@@ -157,43 +157,21 @@ exports.default = _default;
 
 "use strict";
 
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isPersonalAccessTokenPresent = exports.octokitUsingPAT = exports.octokit = exports.personalAccessToken = void 0;
+exports.octokit = exports.personalAccessToken = void 0;
 const github_1 = __webpack_require__(469);
-const core = __importStar(__webpack_require__(470));
 const githubActionsDefaultToken = process.env.GITHUB_TOKEN;
 exports.personalAccessToken = process.env.PERSONAL_ACCESS_TOKEN;
 exports.octokit = (0, github_1.getOctokit)(githubActionsDefaultToken);
-exports.octokitUsingPAT = (0, github_1.getOctokit)(exports.personalAccessToken);
-function isPersonalAccessTokenPresent() {
-    if (!process.env.PERSONAL_ACCESS_TOKEN) {
-        core.setFailed('Please enter a personal access token "PERSONAL_ACCESS_TOKEN" as a environment variable with repo scope for storing signatures in a remote repository!');
-    }
-}
-exports.isPersonalAccessTokenPresent = isPersonalAccessTokenPresent;
+/*export const octokitUsingPAT = getOctokit(personalAccessToken as string)
+
+export function isPersonalAccessTokenPresent() {
+  if (!process.env.PERSONAL_ACCESS_TOKEN) {
+    core.setFailed(
+      'Please enter a personal access token "PERSONAL_ACCESS_TOKEN" as a environment variable with repo scope for storing signatures in a remote repository!'
+    )
+  }
+}*/
 
 
 /***/ }),
--- src/octokit.ts
+++ src/octokit.ts
@@ -1,11 +1,10 @@
 import { getOctokit } from '@actions/github'
-import * as core from '@actions/core'
 
 const githubActionsDefaultToken = process.env.GITHUB_TOKEN
 export const personalAccessToken = process.env.PERSONAL_ACCESS_TOKEN as string
 
 export const octokit = getOctokit(githubActionsDefaultToken as string)
-export const octokitUsingPAT = getOctokit(personalAccessToken as string)
+/*export const octokitUsingPAT = getOctokit(personalAccessToken as string)
 
 export function isPersonalAccessTokenPresent() {
   if (!process.env.PERSONAL_ACCESS_TOKEN) {
@@ -13,4 +12,4 @@ export function isPersonalAccessTokenPresent() {
       'Please enter a personal access token "PERSONAL_ACCESS_TOKEN" as a environment variable with repo scope for storing signatures in a remote repository!'
     )
   }
-}
+}*/
