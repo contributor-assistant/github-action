a9e6bc9a9c9071b228f7f3336f6ff220fff76ea0
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2023-02-09 10:46:19 +0100

    test

--- src/persistence/persistence.ts
+++ src/persistence/persistence.ts
@@ -35,30 +35,35 @@ export async function createFile(contentBinary): Promise<any> {
   })
 }
 
-export async function updateFile(sha: string, claFileContent, reactedCommitters: ReactedCommitterMap): Promise<any> {
-
-    const octokitInstance: InstanceType<typeof GitHub> =
+export async function updateFile(
+  sha: string,
+  claFileContent,
+  reactedCommitters: ReactedCommitterMap
+): Promise<any> {
+  const octokitInstance: InstanceType<typeof GitHub> =
     isRemoteRepoOrOrgConfigured() ? getPATOctokit() : getDefaultOctokitClient()
-    const pullRequestNo = context.issue.number
-    claFileContent?.signedContributors.push(...reactedCommitters.newSigned)
-    let contentString = JSON.stringify(claFileContent, null, 2)
-    let contentBinary = Buffer.from(contentString).toString("base64")
-    await octokitInstance.repos.createOrUpdateFileContents({
-        owner: input.getRemoteOrgName() || context.repo.owner,
-        repo: input.getRemoteRepoName() || context.repo.repo,
-        path: input.getPathToSignatures(),
-        sha,
-        message: input.getSignedCommitMessage()
-          ? input
-              .getSignedCommitMessage()
-              .replace("$contributorName", context.actor)
-              .replace("$pullRequestNo", context.issue.number.toString())
-              .replace("$owner", context.issue.owner)
-              .replace("$repo", context.issue.repo)
-          : `@${context.actor} has signed the CLA from Pull Request #${pullRequestNo}`,
-        content: contentBinary,
-        branch: input.getBranch()
-    })
+
+  const pullRequestNo = context.issue.number
+  console.log(JSON.stringify(context.issue, null, 3))
+  claFileContent?.signedContributors.push(...reactedCommitters.newSigned)
+  let contentString = JSON.stringify(claFileContent, null, 2)
+  let contentBinary = Buffer.from(contentString).toString('base64')
+  await octokitInstance.repos.createOrUpdateFileContents({
+    owner: input.getRemoteOrgName() || context.repo.owner,
+    repo: input.getRemoteRepoName() || context.repo.repo,
+    path: input.getPathToSignatures(),
+    sha,
+    message: input.getSignedCommitMessage()
+      ? input
+          .getSignedCommitMessage()
+          .replace('$contributorName', context.actor)
+          .replace('$pullRequestNo', context.issue.number.toString())
+          .replace('$owner', context.issue.owner)
+          .replace('$repo', context.issue.repo)
+      : `@${context.actor} has signed the CLA from Pull Request #${pullRequestNo}`,
+    content: contentBinary,
+    branch: input.getBranch()
+  })
 }
 
 function isRemoteRepoOrOrgConfigured(): boolean {
